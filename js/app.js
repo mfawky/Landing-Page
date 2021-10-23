f4f8caef0bf1f43931398f330c357386a960398c
m7md fawky <mfawky07@gmail.com>
2021-10-23 07:09:58 +0200

    sections are in active state

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const sections = document.querySelectorAll('section');
 const secOne = document.createElement('li');
 secOne.textContent = "Section 1";              // adds the string {Section 5} to the <li></li>
 navBar.appendChild(secOne);                    // adds Section 1 item to the nav 
-secOne.setAttribute("data-link" , "section1");        // adds an id inside the <li></li> tag
+secOne.setAttribute("data-item" , "section1");        // adds an id inside the <li></li> tag
 secOne.classList.add('nav__item');             // adds a class inside the <li></li> tag
 
 // the same steps will be repeated to the rest of the items
@@ -14,19 +14,19 @@ secOne.classList.add('nav__item');             // adds a class inside the <li></
 const secTwo = document.createElement('li');
 secTwo.textContent = "Section 2";
 navBar.appendChild(secTwo);
-secTwo.setAttribute("data-link" , "section2");
+secTwo.setAttribute("data-item" , "section2");
 secTwo.classList.add('nav__item');
 
 const secThree = document.createElement('li');
 secThree.textContent = "Section 3";
 navBar.appendChild(secThree);
-secThree.setAttribute("data-link" , "section3");
+secThree.setAttribute("data-item" , "section3");
 secThree.classList.add('nav__item');
 
 const secFour = document.createElement('li');
 secFour.textContent = "Section 4";
 navBar.appendChild(secFour);
-secFour.setAttribute("data-link" , "section4");
+secFour.setAttribute("data-item" , "section4");
 secFour.classList.add('nav__item');
 
 
@@ -38,16 +38,16 @@ secFour.classList.add('nav__item');
 
 const items =  document.querySelectorAll(".nav__item");
 
-items.forEach((section)=>{                           // a forEach method to loop the wanted action in all the items
-    section.addEventListener('click',() => {         // a listener that is activated by clicking
+items.forEach(item=>{                           // a forEach method to loop the wanted action in all the items
+    item.addEventListener('click',() => {         // a listener that is activated by clicking
         
-        const element = document.getElementById(section.getAttribute("data-link"));
-        // the previous line makes the item with {data-link} refers to the section whose ID = data-link of the item
-        // you can try it by {console.log} to understand what I mean
-        // So if you clicked on any item it should appear to you in the console what section is corresponding to this item 
-        
+        const element = document.getElementById(item.getAttribute("data-item"));
+        /* the previous line makes the item with {data-link} refers to the section whose ID = data-link of the item
+         you can try it by {console.log} to understand what I mean
+         So if you clicked on any item it should appear to you in the console what section is corresponding to this item 
+         and only the "data-item" is equal to the "data-nav" {both contan the same string}*/
         element.scrollIntoView({
-            //after clicking on any {scrollIntoView} allows you to jump to the corresponding section instantly
+            //after clicking on any item {scrollIntoView} allows you to jump to the corresponding section instantly
             behavior:"smooth",  
             //but we dont want it to move instantly so we used {behavior:"smooth"} to to scroll smoothly 
         });
@@ -60,3 +60,27 @@ items.forEach((section)=>{                           // a forEach method to loop
 //-------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------
 
+// now we will discuss the active state of the sections ------>>>>>>> NOTE : I used MDN as a reference for this part
+
+  const layout =  {
+       root : null,
+       threshold : 0,
+       rootMargin : "0px" 
+   };
+       
+   
+  let observer = new IntersectionObserver(function(entries,observer){ // creating a constructor
+        // forEach method that sees if each entry of entries isInterecting with the viewed secion or not
+        entries.forEach(entry => { 
+          if(!entry.isIntersecting){ 
+                // if its not intersecting {means the value is falsse, and you can test it by connsole.log(entry)} then if it contains "your-active-class" then remove the class from it
+                entry.target.classList.remove("your-active-class");
+          }
+          else{  // and if its intesrecting and dont have "your-active-class" then add the class from it 
+                entry.target.classList.toggle("your-active-class");
+          }
+      })
+  }, layout);
+sections.forEach(section=>{ // forEach method that observes each section of sections to see if it enters the viewport or not 
+    observer.observe(section);
+});
